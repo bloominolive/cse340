3276fb66e08a1658c52c743d975c0ee334ad71c0
bloominolive <bloomin.olive88@gmail.com>
2023-04-23 06:10:17 -0400

    enhancement 1 project begin

---
+++ phpmotors/common/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home | PHP Motors</title>
+    <link href="/phpmotors/css/style.css" type="text/css" rel="stylesheet" media="screen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <div id="header">
+                <img id= "logo" src="/phpmotors/images/site/logo.png" alt="php motors logo" href="/phpmotors/home.php">
+                <a href="#" id="my-account">My Account</a>
+            </div>
+        <nav>
+            <?php include $_SERVER['DOCUMENT-ROOT'] . '/phpmotos/common.nav.php'; ?>
+        </nav>
+        <main>
